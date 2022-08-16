52a128d6008c979691cb535c7300e5e3209914dd
Yuumin97 <fiesportlmtc@gmail.com>
2022-08-16 11:39:31 +0700

    k

---
+++ src/Copy.java
@@ -0,0 +1,36 @@
+
+import java.io.*;
+
+public class Copy {
+    public static void main(String[] args)
+            throws IOException
+    {
+        FileInputStream source = null;
+        FileOutputStream target = null;
+
+        try {
+
+            source = new FileInputStream(
+                    "D:\\MD2\\Bai16\\BT1\\BT1\\src\\source file.txt");
+
+            target = new FileOutputStream(
+                    "D:\\MD2\\Bai16\\BT1\\BT1\\src\\target file.txt");
+
+            int c;
+            while ((c = source.read()) != -1) {
+                target.write(c);
+            }
+            System.out.println("Đã copy thành công");
+        }
+        finally {
+            if (source != null) {
+            }else {
+                source.close();
+            }
+            if (target != null) {
+            }else {
+                target.close();
+            }
+        }
+    }
+}
